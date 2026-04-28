dbd4e4265911bb4bf830f286021e0bde8fe431f5
Thio Saputra <132740827+flessan@users.noreply.github.com>
2026-04-28 13:59:56 +0700

    UPD Main

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+    "fmt"
+    "os"
+
+    "github.com/flessan/nurlaily/cmd"
+)
+
+func main() {
+    if err := cmd.Execute(); err != nil {
+        fmt.Fprintf(os.Stderr, "\033[31mError:\033[0m %v\n", err)
+        os.Exit(1)
+    }
+}package main
+
 import "github.com/flessan/nurlaily/cmd"
 
 func main() {
