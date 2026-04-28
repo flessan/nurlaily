c26bdf5a3cefcd1f1a8c8c01ac25c25036a13924
Thio Saputra <132740827+flessan@users.noreply.github.com>
2026-04-28 14:01:54 +0700

    Create Model

---
+++ internal/model/model.go
@@ -0,0 +1,35 @@
+package model
+
+import "html/template"
+
+type EntryData struct {
+    Time       string
+    Content    template.HTML
+    ContentRaw string
+    Mood       string
+    Tags       []string
+    WordCount  int
+    ReadTime   string
+}
+
+type DayData struct {
+    Date      string
+    DateHuman string
+    Entries   []EntryData
+    Count     int
+}
+
+type TagInfo struct {
+    Name  string
+    Count int
+}
+
+type PageData struct {
+    Days         []DayData
+    TotalDays    int
+    TotalEntries int
+    TotalWords   int
+    AllTags      []TagInfo
+    Title        string
+    GeneratedAt  string
+}
